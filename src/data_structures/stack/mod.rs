be3bf57fce2170b07201258af7244089d625698e
r9n9ga7 <r9n9ga7@gmail.com>
2026-01-30 15:52:41 +0900

    added a simple stack implementation based on a vector

---
+++ src/data_structures/stack/mod.rs
@@ -0,0 +1,126 @@
+#[allow(dead_code)]
+struct Stack<T> {
+    arr: Vec<T>,
+}
+
+#[allow(dead_code)]
+impl<T> Stack<T> {
+    fn new() -> Self {
+        return Self { arr: Vec::new() };
+    }
+
+    fn push(&mut self, value: T) {
+        self.arr.push(value);
+    }
+
+    fn pop(&mut self) -> Option<T> {
+        return self.arr.pop();
+    }
+
+    fn top(&self) -> Option<&T> {
+        return self.arr.last();
+    }
+
+    fn is_empty(&self) -> bool {
+        return self.arr.is_empty();
+    }
+
+    fn size(&self) -> usize {
+        return self.arr.len();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn push_i64() {
+        let mut stack = Stack::<i64>::new();
+        stack.push(2);
+        assert_eq!(stack.arr[0], 2);
+    }
+
+    #[test]
+    fn push_string() {
+        let mut stack = Stack::<String>::new();
+        stack.push("test".to_string());
+        assert_eq!(stack.arr[0], "test".to_string());
+    }
+
+    #[test]
+    fn pop_i64() {
+        let mut stack = Stack::<i64>::new();
+        stack.push(2);
+
+        let value = stack.pop();
+
+        assert_eq!(value, Some(2));
+        assert!(stack.arr.is_empty())
+    }
+
+    #[test]
+    fn pop_string() {
+        let mut stack = Stack::<String>::new();
+        stack.push("test".to_string());
+
+        let value = stack.pop();
+
+        assert_eq!(value, Some("test".to_string()));
+        assert!(stack.arr.is_empty())
+    }
+
+    #[test]
+    fn push_pop_order() {
+        let mut stack = Stack::<i64>::new();
+        stack.push(1);
+        stack.push(2);
+
+        {
+            let value = stack.pop();
+            assert_eq!(value, Some(2));
+        }
+
+        {
+            let value = stack.pop();
+            assert_eq!(value, Some(1));
+        }
+    }
+
+    #[test]
+    fn top_i64() {
+        let mut stack = Stack::<i64>::new();
+        stack.push(10);
+
+        let value = stack.top().unwrap();
+        assert_eq!(*value, 10);
+        assert_eq!(stack.arr.len(), 1);
+    }
+
+    #[test]
+    fn is_empty() {
+        let mut stack = Stack::<i64>::new();
+
+        assert!(stack.is_empty());
+
+        stack.push(10);
+
+        assert!(!stack.is_empty());
+
+        stack.pop();
+
+        assert!(stack.is_empty());
+    }
+
+    #[test]
+    fn size() {
+        let mut stack = Stack::<i64>::new();
+
+        assert_eq!(stack.size(), 0);
+
+        stack.push(10);
+        stack.push(10);
+
+        assert_eq!(stack.size(), 2);
+    }
+}
